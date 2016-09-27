309c71378fca87990ba55be981a4abbd2aedce04
Paweł Kata <gmabber@gmail.com>
2016-09-27 10:06:42 +0200

    Swift 3 support.

--- SwiftRangeSlider/RangeSliderTrackLayer.swift
+++ SwiftRangeSlider/RangeSliderTrackLayer.swift
@@ -12,24 +12,24 @@ import QuartzCore
 class RangeSliderTrackLayer: CALayer {
   weak var rangeSlider: RangeSlider?
   
-  override func drawInContext(ctx: CGContext) {
+  override func draw(in ctx: CGContext) {
     if let slider = rangeSlider {
       // Clip
       let cornerRadius = bounds.height * slider.curvaceousness / 2.0
       let path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
-      CGContextAddPath(ctx, path.CGPath)
+      ctx.addPath(path.cgPath)
       
       // Fill the track
-      CGContextSetFillColorWithColor(ctx, slider.trackTintColor.CGColor)
-      CGContextAddPath(ctx, path.CGPath)
-      CGContextFillPath(ctx)
+      ctx.setFillColor(slider.trackTintColor.cgColor)
+      ctx.addPath(path.cgPath)
+      ctx.fillPath()
       
       // Fill the highlighted range
-      CGContextSetFillColorWithColor(ctx, slider.trackHighlightTintColor.CGColor)
+      ctx.setFillColor(slider.trackHighlightTintColor.cgColor)
       let lowerValuePosition = CGFloat(slider.positionForValue(slider.lowerValue))
       let upperValuePosition = CGFloat(slider.positionForValue(slider.upperValue))
       let rect = CGRect(x: lowerValuePosition, y: 0.0, width: upperValuePosition - lowerValuePosition, height: bounds.height)
-      CGContextFillRect(ctx, rect)
+      ctx.fill(rect)
     }
   }
 }
